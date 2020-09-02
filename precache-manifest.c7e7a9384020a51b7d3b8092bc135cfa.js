f7fbf17e5ff4fe87d5a9e1061f4d61b8ef0205e4
akuma1999 <santoruy1999@gmail.com>
2020-09-02 21:15:36 +0700

    Updates

---
+++ precache-manifest.c7e7a9384020a51b7d3b8092bc135cfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17cfc557c9423996c8e4b3d58abf4fc6",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "81302a20b2c6d9802482",
+    "url": "/calendar/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e248a95f79c0495aa075",
+    "url": "/calendar/static/js/2.0c9143c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calendar/static/js/2.0c9143c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81302a20b2c6d9802482",
+    "url": "/calendar/static/js/main.cc912ae1.chunk.js"
+  },
+  {
+    "revision": "7d0fb5c2975c2066ee7b",
+    "url": "/calendar/static/js/runtime-main.914f0415.js"
+  }
+]);
